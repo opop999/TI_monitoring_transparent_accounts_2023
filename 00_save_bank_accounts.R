a504e931ebf7514c658d746822c3e5f154a827de
Ondrej Pekacek <ondrej.pekacek@fsv.cuni.cz>
2022-11-16 16:04:25 +0100

    feat: add extra control conditions and simplify the existing ones

--- 00_save_bank_accounts.R
+++ 00_save_bank_accounts.R
@@ -1,5 +1,4 @@
-# To improve readability of the main script and its length, this script is made
-# to be modified. We can add and remove the monitored bank accounts pages and then
+# We can add and remove the monitored bank accounts pages here and then
 # we save the list to a rds file, which is read to the main extraction script.
 
 main_dir <- "data"
@@ -12,10 +11,10 @@ if (!dir.exists(main_dir)) {
 
 
 # Initiate an empty list, serving as a list of all of the lists
-all_accounts_list <- vector(mode = "list", length = 4L) 
-all_accounts_list <- setNames(all_accounts_list, c("fio", "kb", "csob", "cs"))
+all_accounts_list <- vector(mode = "list", length = 4L)
+all_accounts_list <- setNames(all_accounts_list, c("fio", "kb", "csob", "csas"))
 
-# 1.1. This list is for FIO accounts --------------------------
+# 1. This list is for FIO accounts ---------------------------------------------
 
 all_accounts_list[["fio"]] <- list(
   names = c(
@@ -29,8 +28,7 @@ all_accounts_list[["fio"]] <- list(
 )
 
 
-# 
-# # 3.1 This list is for accounts extracted through KB API -----
+# # 2. This list is for accounts extracted through Komercni Banka API -----
 # all_accounts_list[["kb"]] <- list(
 #   names = c(
 #     "ano_2011"
@@ -42,12 +40,12 @@ all_accounts_list[["fio"]] <- list(
 #     "4090453" # This is formatting for direct KB URL
 #   )
 # )
-# 
-# 
-# 
-# 
-# # 4.1 This list is for accounts extracted through CSOB API --------------------------
-# 
+#
+#
+#
+#
+# # 3. This list is for accounts extracted through CSOB API --------------------------
+#
 # all_accounts_list[["csob"]] <- list(
 #   names = c(
 #     "kscm"
@@ -56,17 +54,28 @@ all_accounts_list[["fio"]] <- list(
 #     "217343303"
 #   )
 # )
-# 
-# 
-# 
-# # 5.1 This list is for accounts extracted through Ceska Sporitelna (CS) API --------------------------
-# 
-# all_accounts_list[["cs"]] <- list(
-#   names = c(),
-#   numbers = c()
-# )
+#
+#
+#
+
+# 4.This list is for accounts extracted through Ceska Sporitelna (CS) API ------
+
+all_accounts_list[["csas"]] <- list(
+  names = c(
+    "ivo_mares",
+    "danuse_nerudova",
+    "alena_vitaskova",
+    "tomas_zima"
+  ),
+  numbers = c(
+    "000000-4672082043",
+    "000000-4776908073",
+    "000000-4639079053",
+    "000000-4758343013"
+  )
+)
 
 
 
-# Save the list of lists for all of the accounts --------------------------
-saveRDS(all_accounts_list, paste0(main_dir, "/list_of_monitored_accounts.rds")) 
+# 5. Save the list of lists for all of the accounts ----------------------------
+saveRDS(all_accounts_list, paste0(main_dir, "/list_of_monitored_accounts.rds"))
